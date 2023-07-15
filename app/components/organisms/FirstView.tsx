500c95c18f555a66351dc669bc3da720750b0c23
ReoYabiku <alcremikyu.z@gmail.com>
2023-07-15 19:00:19 +0900

    reactプロジェクトからの移植

---
+++ app/components/organisms/FirstView.tsx
@@ -0,0 +1,30 @@
+import style from './FirstView.module.css';
+import NewsCard from '../molecules/NewsCard';
+
+const FirstView = () => {
+    return (
+        <section className={style.first_view}>
+            <img className={style.image} src="./first_view.png" alt="安田講堂" />
+
+            {/* 三角形 */}
+            <div className={style.triangle1}></div>
+            <div className={style.triangle2}></div>
+
+            {/* カード */}
+            <NewsCard
+                message1='【最新イベント情報】'
+                message2='一泊二日 勉強合宿開催決定'
+                message3='参加申し込みはこちら ▶︎'
+                top='20px'
+                left='20px'
+            />
+
+            <h2 className={style.copy}>
+                沖縄から東大を、<br />
+                日常に
+            </h2>
+        </section>
+    );
+}
+
+export default FirstView;
---
+++ app/components/organisms/Header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import style from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <Link href={'/'} className={style.logo}>
+        <div className={style.logo_img_wrapper}>
+          <img src="ALOHA_logo.png" alt="ALOHAのロゴ" width={45} height={45} />
+        </div>
+        <div>
+          <h1 className={style.title}>ALOHA</h1>
+          <p className={style.subtitle}>沖縄から東大を、日常に</p>
+        </div>
+      </Link>
+      <div className={style.nav}>
+        <Link href={'/introduction'} className={style.other_pages}>団体紹介</Link>
+        <Link href={'/members'} className={style.other_pages}>メンバー紹介</Link>
+        <Link href={'/documents'} className={style.other_pages}>資料一覧</Link>
+        <Link href={'/activities'} className={style.other_pages}>活動実績</Link>
+      </div>
+    </header>
+  );
+}
