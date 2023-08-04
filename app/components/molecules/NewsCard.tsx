e2d94e8db0d9c93f013ff9dd70a02d279d219b00
ReoYabiku <alcremikyu.z@gmail.com>
2023-08-04 18:55:47 +0900

    first view sp

--- app/components/molecules/NewsCard.tsx
+++ app/components/molecules/NewsCard.tsx
@@ -4,20 +4,18 @@ type Props = {
 	message1: string,
 	message2: string,
 	message3: string,
-	top: string,
-	left: string
 };
 
-const NewsCard = ({message1, message2, message3, top, left}: Props) => {
+const NewsCard = ({message1, message2, message3}: Props) => {
 	return (
-		<div className={style.card} style={{top: top, left: left}}>
+		<div className={style.card}>
 			<div className={style.top}></div>
 			<div className={style.content}>
-				<p>&nbsp;&nbsp;{message1}</p>
-				<p>&nbsp;&nbsp;{message2}</p>
+				<p className={style.text}>&nbsp;&nbsp;{message1}</p>
+				<p className={style.text}>&nbsp;&nbsp;{message2}</p>
 			</div>
 			<div className={style.bottom}>
-				<p>&nbsp;&nbsp;{message3}</p>
+				<p className={style.text}>&nbsp;&nbsp;{message3}</p>
 			</div>
 		</div>
 	);
--- app/components/organisms/FirstView.tsx
+++ app/components/organisms/FirstView.tsx
@@ -2,6 +2,7 @@ import style from './FirstView.module.css';
 import NewsCard from '../molecules/NewsCard';
 import Image from 'next/image';
 import firstViewPic from './first_view.jpg';
+import firstViewSPPic from './first_view_sp.png';
 
 const FirstView = () => {
 	return (
@@ -10,6 +11,12 @@ const FirstView = () => {
 				className={style.image}
 				src={firstViewPic}
 				alt="安田講堂"
+			/>
+			<Image
+				className={style.imageSP}
+				src={firstViewSPPic}
+				fill={true}
+				alt='安田講堂'
 			/>
 
 			{/* 三角形 */}
@@ -21,14 +28,17 @@ const FirstView = () => {
 				message1='【最新イベント情報】'
 				message2='一泊二日 勉強合宿開催決定'
 				message3='参加申し込みはこちら ▶︎'
-				top='20px'
-				left='20px'
 			/>
 
 			<h2 className={style.copy}>
 				沖縄から東大を、<br />
 				日常に
 			</h2>
+
+			<div className={style.border}></div>
+
+			<h2 className={style.copySP1}>沖縄から東大を、</h2>
+			<h2 className={style.copySP2}>日常に</h2>
 		</section>
 	);
 }
