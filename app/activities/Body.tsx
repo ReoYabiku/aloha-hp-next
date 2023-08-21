3a730b595436f12f5377364d034fe90318344094
Tsunekazu Omija <mijamijaut@g.ecc.u-tokyo.ac.jp>
2023-08-21 17:53:37 +0900

    activityを作った

---
+++ app/activities/Body.tsx
@@ -0,0 +1,105 @@
+import Image from 'next/image';
+import style from  './Body.module.css';
+import GatheringPic from './東大生交流会 1.png';
+import { StaticImageData } from 'next/image';
+
+type ActivityProps = {
+    name: string
+    description: JSX.Element
+    img: StaticImageData
+    last: boolean
+}
+
+export default function Activities() {
+    const activityDetails: ActivityProps[] = [
+        {
+            name: '出張講演会',
+            description:
+                <>
+                　出張講演会の説明。実施場所、企画内容、参加人数等を記載する。
+                団体としての信頼獲得を目的としているので、企画の目的やその効果を表現できるような説明文を記載したい。
+                見た目を整えるために、写真と同じ幅まで文章が埋まっていると、なお望ましい。<br/>
+                　このサムネイルをクリックするとイベントの詳細ページに遷移するので、興味を引きたい。
+                </>,
+            img: GatheringPic,
+            last: false,
+        },
+        {
+            name: '東大生交流会',
+            description:
+                <>
+                　東大生交流会の説明。実施場所、企画内容、参加人数等を記載する。
+                団体としての信頼獲得を目的としているので、企画の目的やその効果を表現できるような説明文を記載したい。
+                見た目を整えるために、写真と同じ幅まで文章が埋まっていると、なお望ましい。<br/>
+                　このサムネイルをクリックするとイベントの詳細ページに遷移するので、興味を引きたい。
+                </>,
+            img: GatheringPic,
+            last: false,
+        },
+        {
+            name: 'オンライン面談',
+            description:
+                <>
+                　オンライン面談の説明。実施場所、企画内容、参加人数等を記載する。
+                団体としての信頼獲得を目的としているので、企画の目的やその効果を表現できるような説明文を記載したい。
+                見た目を整えるために、写真と同じ幅まで文章が埋まっていると、なお望ましい。<br/>
+                　このサムネイルをクリックするとイベントの詳細ページに遷移するので、興味を引きたい。
+                </>,
+            img: GatheringPic,
+            last: true,
+        }
+    ]
+
+    return (
+        <section className={style.background}>
+            {
+                activityDetails.map((props) => (
+                    <div>
+                        <div className={style.container}>
+                            <Image
+                                src={props.img}
+                                width={376}
+                                height={299}
+                                alt={props.name}
+                            />
+                            <div className={style.child}>
+                                <h1 className={style.title}>{props.name}</h1>
+                                <p className={style.text}>
+                                    {props.description}
+                                </p>
+                            </div>
+                        </div>
+                        {props.last ? false : <div className={style.bar}/>}
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
+
+{/*
+export default function Test() {
+    return(
+        <section className={style.background}>
+            <div className={style.container}>
+                <Image
+                    src={GatheringPic}
+                    width={376}
+                    height={299}
+                    alt="Lecture"
+                />
+                <div className={style.child}>
+                    <h1 className={style.title}>出張講演会</h1>
+                    <p className={style.text}>
+                    　出張講演会の説明。実施場所、企画内容、参加人数等を記載する。
+                    団体としての信頼獲得を目的としているので、企画の目的やその効果を表現できるような説明文を記載したい。
+                    見た目を整えるために、写真と同じ幅まで文章が埋まっていると、なお望ましい。<br/>
+                    　このサムネイルをクリックするとイベントの詳細ページに遷移するので、興味を引きたい。
+                    </p>
+                </div>
+            </div>
+            <div className={style.bar}/>
+        </section>
+    )
+}
+*/}
--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -1,4 +1,5 @@
 import Title from '../components/organisms/Title';
+import Body from './Body';
 
 export default function Activities() {
   return (
@@ -7,6 +8,8 @@ export default function Activities() {
         subtitle='活動実績'
         title='Activities'
         description='活動実績に関するいい感じの文章。長めの文章にしたいけど書くことないから適当に引き伸ばしています。'
+      />
+      <Body
       />
     </main>
   );
