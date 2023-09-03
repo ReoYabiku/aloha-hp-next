df22e1e4683b785175d7110f43bfc1b5e3a6680a
Tsunekazu Omija <mijamijaut@g.ecc.u-tokyo.ac.jp>
2023-09-03 11:25:30 +0900

    スマホ画面では電話番号が改行されるようにした

--- app/(footer)/GroupInfo.tsx
+++ app/(footer)/GroupInfo.tsx
@@ -5,7 +5,7 @@ export default function GroupInfo() {
     <div className={style.container}>
       <h2 className={style.title}>ALOHA</h2>
       <h3 className={style.subtitle}>沖縄から東大を、<wbr />日常に</h3>
-      <p className={style.text}>お問い合わせ：080-1234-1234（伊礼）</p>
+      <p className={style.text}>お問い合わせ：<br className={style.sp_br}/>080-1234-1234（伊礼）</p>
     </div>
   );
 }
