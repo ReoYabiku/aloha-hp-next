57a5d5bc3cd8b13c60693e2634a280204d385e51
ReoYabiku <alcremikyu.z@gmail.com>
2023-12-18 02:17:45 +0900

    面談と講演会

--- app/(user)/articles/[id]/page.tsx
+++ app/(user)/articles/[id]/page.tsx
@@ -1,9 +1,15 @@
 import Sample from './Sample';
+import OnlineInterview from './OnlineInterview';
+import VisitingLecture from './VisitingLecture';
 
 export default function Article({ params }: { params: { id: string } }) {
   switch (params.id) {
     case "sample":
       return <Sample />;
+    case "online-interview":
+      return <OnlineInterview />
+    case "visiting-lecture":
+      return <VisitingLecture />
     case "hoge":
       return <div>hogehoge</div>;
   }
