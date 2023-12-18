2a40cbb8c06d2ed2d6138d4e30423d421efe4775
ReoYabiku <alcremikyu.z@gmail.com>
2023-12-18 15:54:08 +0900

    add images to study camp article

--- app/(user)/articles/[id]/StudyCamp.tsx
+++ app/(user)/articles/[id]/StudyCamp.tsx
@@ -42,15 +42,47 @@ export default function StudyCamp() {
             <p>以下では合宿の内容や参加生徒の様子についてご紹介します。</p>
             <h3 className={style.text_headline}>合宿の流れ</h3>
             <p>今回の合宿は以下のスケジュールで実施しました。</p>
+            <Image
+              className={style.image}
+              width={780}
+              height={400}
+              style={{objectFit:"cover"}}
+              alt='勉強合宿の二日間のスケジュール'
+              src={"https://alohahp.s3.ap-northeast-1.amazonaws.com/articles/study-camp1.png"}
+            />
             <h3 className={style.text_headline}>対抗戦</h3>
+            <Image
+              className={style.image}
+              width={780}
+              height={400}
+              style={{objectFit:"cover"}}
+              alt='対抗戦に取り組む生徒達'
+              src={"https://alohahp.s3.ap-northeast-1.amazonaws.com/articles/study-camp2.jpg"}
+            />
             <p>対抗戦では、チームに分かれて実際の入試問題を含んだ数学の問題を解き、その正解数を競い合うゲームを実施しました。</p>
             <p>チーム内の親睦を深めながら、他の人の解き方や思考を吸収する事を目的としました。1人で勉強している時にはなかった発想に出会い、同じレベルを目指す仲間から良い刺激を受ける機会になったと思います。</p>
             <h3 className={style.text_headline}>特別授業　数学＆国語</h3>
+            <Image
+              className={style.image}
+              width={780}
+              height={400}
+              style={{objectFit:"cover"}}
+              alt='数学の特別授業の様子'
+              src={"https://alohahp.s3.ap-northeast-1.amazonaws.com/articles/study-camp3.jpg"}
+            />
             <p>特別授業では、開邦と薬科の先生方をお招きして実際の入試問題を扱った授業を行いました。</p>
             <p>数学の授業では、「悩んでいる時間」にこそ価値があり数学的思考を鍛えられる時間だといった、普段の授業では教わらない難関大数学への取り組み方をご講義していただきました。</p>
             <p>国語の授業では、東大と京大の入試問題の比較を通して大学入試で求められる現代文の能力についてご講義していただきました。大学設立の変遷や問題の題材の選び方などから、それぞれの大学の求める人材について紹介していただきました。</p>
             <p>高校１年生、２年生の受験に不安を感じている生徒達に向けて、先生方から大学受験に向けた熱いエールもいただきました。</p>
             <h3 className={style.text_headline}>暗記演習</h3>
+            <Image
+              className={style.image}
+              width={780}
+              height={400}
+              style={{objectFit:"cover"}}
+              alt='暗記演習の様子'
+              src={"https://alohahp.s3.ap-northeast-1.amazonaws.com/articles/study-camp4.jpg"}
+            />
             <p>10分間の英語例文暗記と確認テストを繰り返し、暗記力と集中力の限界にチャレンジする特訓を行いました。</p>
             <p>生徒達は短時間での暗記に苦戦していましたが、回数をこなすほどに集中力が上がっていく様子が見受けられました。暗記が苦手な生徒の壁を突破するきっかけになってくれたら良いと思います。</p>
             <h3 className={style.text_headline}>東大生との座談会</h3>
