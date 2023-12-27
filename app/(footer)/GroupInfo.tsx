1aa179435551d978ffd25e7c7291222417acce27
ReoYabiku <alcremikyu.z@gmail.com>
2023-12-27 16:31:30 +0900

    yabiku phone

--- app/(footer)/GroupInfo.tsx
+++ app/(footer)/GroupInfo.tsx
@@ -5,7 +5,7 @@ export default function GroupInfo() {
     <div className={style.container}>
       <h2 className={style.title}>ALOHA</h2>
       <h3 className={style.subtitle}>沖縄から東大を、<wbr />日常に</h3>
-      <p className={style.text}>お問い合わせ：<br className={style.sp_br}/>080-9523-8132（伊礼）</p>
+      <p className={style.text}>お問い合わせ：<br className={style.sp_br}/>080-9106-6033（屋比久）</p>
     </div>
   );
 }
