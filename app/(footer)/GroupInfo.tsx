343f060d7fab5a1c34d7a41b90e50c36414b937f
ReoYabiku <alcremikyu.z@gmail.com>
2023-12-27 16:33:24 +0900

    e-mail

--- app/(footer)/GroupInfo.tsx
+++ app/(footer)/GroupInfo.tsx
@@ -5,7 +5,8 @@ export default function GroupInfo() {
     <div className={style.container}>
       <h2 className={style.title}>ALOHA</h2>
       <h3 className={style.subtitle}>沖縄から東大を、<wbr />日常に</h3>
-      <p className={style.text}>お問い合わせ：<br className={style.sp_br}/>080-9106-6033（屋比久）</p>
+      <p className={style.text}>電話番号：<br className={style.sp_br}/>080-9106-6033（屋比久）</p>
+      <p className={style.text}>メールアドレス：<br className={style.sp_br}/>aloha.edu23@gmail.com</p>
     </div>
   );
 }
